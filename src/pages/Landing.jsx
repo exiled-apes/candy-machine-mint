e293b3cd5d2b5ac5a4636a1d9ce1dce8bc0c3f19
sgf-sky <skywalker@sgfunited.com>
2021-09-14 21:08:24 +0530

    landing page 90% complete

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect, useRef } from "react";
 import GLOBE from "vanta/dist/vanta.globe.min";
 import Ticker from "react-ticker";
 
+import Flag from "../assets/landing/Flag.png";
+import Spaceship from "../assets/landing/spaceship.png";
+import Avatar from "../assets/landing/team-avatar.png";
+
 function Landing() {
   const [effect, setEffect] = useState(null);
   const effectRef = useRef(null);
@@ -37,7 +41,7 @@ function Landing() {
         <div>
           <h1 className=" text-7xl font-black italic">
             1st COMMUNITY-OWNED{" "}
-            <span className="text-primary-dark relative glow">
+            <span className="text-primary-light relative glow">
               SCI-FI MEDIA HOUSE{" "}
             </span>{" "}
             🔭 BUILT ON SOLANA
@@ -62,7 +66,101 @@ function Landing() {
           </Ticker>
         </div>
       </div>
-      <div className="h-screen bg-red-700"></div>
+      <div
+        className="h-screen bg-center bg-cover flex items-center px-30 relative"
+        id="landing-section-2"
+      >
+        <img src={Flag} className="absolute top-0 right-0" />
+        <div>
+          <h1 className="text-primary-light text-5xl font-black italic glow">
+            AHOY SPACE CADETS
+          </h1>
+          <div className="font-orb text-3xl text-white mt-10 space-y-2 uppercase">
+            <p>
+              Welcome To{" "}
+              <span className="text-primary-dark font-medium">
+                SGF’s Mission Control
+              </span>{" "}
+              room.
+            </p>
+            <p>
+              Here <span className="font-medium">You</span> will{" "}
+              <span className="font-medium">find</span> everything to{" "}
+              <span className="font-medium">get started</span>.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div
+        className="h-screen bg-center bg-cover relative  py-28 pl-36"
+        id="vision"
+      >
+        <div className="grid grid-cols-2">
+          <div className="text-white">
+            <h1 className="text-primary-light glow text-5xl font-black italic">
+              SGF's Vision
+            </h1>
+            <div className="uppercase font-orb text-3xl mt-10 font-medium">
+              <p>
+                To build <br />
+                the Sci-Fi media house <br />
+                21'st century deserves. <br />
+                <br />
+                Community owned from the start.
+              </p>
+            </div>
+          </div>
+          <img src={Spaceship} className="justify-self-end" />
+        </div>
+        <div>
+          <p className="mt-10 text-white text-3xl font-orb uppercase">
+            Intellectual property DROPs planned:
+            <br />
+            <span className="font-medium">GENSIS 10k Gen-1 CYBORG</span>
+          </p>
+
+          <div className="mt-10 space-x-3 font-orb">
+            <button className="btn-primary font-black">GEN-1 NFT DROP</button>
+            <a href="#" className="btn-secondary font-black">
+              JOIN DISCORD
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="h-screen bg-center bg-cover pt-20 relative" id="team">
+        <div className="bg-black bg-opacity-40 h-full absolute inset-0 z-0"></div>
+        <div className="relative z-10">
+          <h1 className="uppercase text-primary-light glow font-black italic text-5xl text-center">
+            Stellar team
+          </h1>
+          <div className="max-w-4xl mx-auto">
+            <div className="grid grid-cols-3 font-orb text-white mt-20">
+              <div className="space-y-2">
+                <img src={Avatar} alt="" className="mx-auto" />
+                <p className="text-center">@kayden</p>
+              </div>
+              <div className="space-y-2">
+                <img src={Avatar} alt="" className="mx-auto" />
+                <p className="text-center">@nonfungibleprime</p>
+              </div>
+              <div className="space-y-2">
+                <img src={Avatar} alt="" className="mx-auto" />
+                <p className="text-center">@greywolf</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 font-orb text-white mt-16">
+              <div className="space-y-2">
+                <img src={Avatar} alt="" className="mx-auto" />
+                <p className="text-center">@alphatron</p>
+              </div>
+              <div className="space-y-2">
+                <img src={Avatar} alt="" className="mx-auto" />
+                <p className="text-center">@skywalker</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
