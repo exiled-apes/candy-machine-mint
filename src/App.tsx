7a16cbf093ab79a60d1a7a8e1de8f54f70dcf3ac
mcai1 <cai.matt308@gmail.com>
2021-09-15 01:42:57 -0700

    bumping timeout

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const connection = new anchor.web3.Connection(rpcHost);
 
 const startDateSeed = parseInt(process.env.REACT_APP_CANDY_START_DATE!, 10);
 
-const txTimeout = 30000; // milliseconds (confirm this works for your project)
+const txTimeout = 50000; // milliseconds (confirm this works for your project)
 
 const App = () => {
   const endpoint = useMemo(() => clusterApiUrl(network), []);
