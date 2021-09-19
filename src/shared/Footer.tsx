679c7a8013f3623f59834eae0ab81f3d606b23ac
willflemmer <60981618+willflemmer@users.noreply.github.com>
2021-09-19 15:43:13 +0100

    style: update footer

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -1,5 +1,5 @@
-import discordIcon from '../media/discord-icon.png'
-import twitterIcon from '../media/twitter-icon.png'
+import discordIcon from '../media/discord-icon.svg'
+import twitterIcon from '../media/twitter-icon.svg'
 
 interface IProps {
   image: string,
@@ -10,22 +10,22 @@ interface IProps {
 const Footer = ({image, disclaimer, roadmapLink, faqLink}: IProps) => {
 
   return (
-    <div className='frow' style={{width: '60%', justifyContent: 'space-between'}}>
+    <div className='frow' style={{width: '60%', justifyContent: 'space-between', padding: '64px 0px'}}>
       <div className='flex-column'>
-        <img src ={image} style={{width: '30px'}}/>
+        <img src ={image} style={{width: '30px', paddingBottom: '12px'}}/>
         {disclaimer}
       </div>
-      <div className='flex-column' style={{width: '20%'}}>
-        <div className='frow' style={{justifyContent: 'space-between'}}>
+      <div className='flex-column' style={{width: '15%'}}>
+        <div className='frow' style={{justifyContent: 'flex-end',  paddingBottom: '12px'}}>
           <a>
-            <img src={discordIcon} style={{width: '30px'}}/>
+            <img  src={discordIcon} style={{width: '20px', marginRight: '25px'}}/>
           </a>
           <a>
-            <img src={twitterIcon} style={{width: '30px'}}/>
+            <img src={twitterIcon} style={{width: '20px'}}/>
           </a>          
         </div>
-        <div className='frow' style={{justifyContent: 'space-between'}}>
-          <a href={roadmapLink}>Roadmap</a>
+        <div className='frow' style={{justifyContent: 'flex-end'}}>
+          <a href={roadmapLink} style={{marginRight: '25px'}}>Roadmap</a>
           <a href={faqLink}>FAQ</a>
         </div>
       </div>
