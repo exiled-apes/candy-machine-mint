849fadeeed8f1f47591d420ac82294424296c785
Andréas Brey <anbr@4js.com>
2022-01-21 16:18:33 +0100

    increased tx timeout duration otherwise it could always fail if binding CM over public solana mainnet-beta network without custom RPC node

--- src/connection.tsx
+++ src/connection.tsx
@@ -339,7 +339,7 @@ import {
     return new Date().getTime() / 1000;
   };
   
-  const DEFAULT_TIMEOUT = 15000;
+  const DEFAULT_TIMEOUT = 30000;
   
   export async function sendSignedTransaction({
     signedTransaction,
