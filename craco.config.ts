d2a53da3ee2a6ddd07390f54e3c4d35de44f1722
steveluscher <me+github@steveluscher.com>
2022-10-21 20:29:28 -0700

    Upgrade build system

---
+++ craco.config.ts
@@ -0,0 +1,10 @@
+const config = {
+  babel: {
+    plugins: [
+      "@babel/plugin-proposal-nullish-coalescing-operator",
+      "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-transform-shorthand-properties",
+    ],
+  },
+};
+export default config;
